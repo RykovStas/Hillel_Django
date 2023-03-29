e95b681a56926ceb42b6f0d260c790ba052f0d1d
Stas <stasyan_2003@icloud.com>
2023-03-29 18:22:30 +0300

    Update Persons

--- user/urls.py
+++ user/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', views.index, name='person'),
     path('show_persons/', views.show_persons, name='show_persons'),
     path('post_person/', views.post_person, name='register_user'),
+    path('update_person/<int:pk>/', views.person_update, name='person_update'),
 ]
--- user/views.py
+++ user/views.py
@@ -22,3 +22,15 @@ def post_person(request):
 
     return render(request, 'user/post_person.html', {'form': user_form})
 
+
+def person_update(request, pk):
+    obj = get_object_or_404(UserModel, pk=pk)
+    if request.method == 'POST':
+        user_form = PersonForm(request.POST, instance=obj)
+        if user_form.is_valid():
+            obj = user_form.save()
+            return redirect('triangle:person')
+    else:
+        user_form = PersonForm(instance=obj)
+
+    return render(request, 'user/person_update.html', {'form': user_form, 'obj': obj})
