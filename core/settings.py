0b23ebcfbb34a97120101cfcdb3102f0d7819043
Stas <stasyan_2003@icloud.com>
2023-04-04 22:46:02 +0300

    Create LogMiddlewar

--- core/settings.py
+++ core/settings.py
@@ -58,6 +58,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'user.middleware.LogMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
